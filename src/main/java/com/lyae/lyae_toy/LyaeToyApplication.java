0cc61044a5c1ec4fe3c6cb2a9502bf14fb6bc6c3
lyae <lyaesley@gmail.com>
2023-08-23 23:16:29 +0900

    Initial commit

---
+++ src/main/java/com/lyae/lyae_toy/LyaeToyApplication.java
@@ -0,0 +1,13 @@
+package com.lyae.lyae_toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyaeToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyaeToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyae/lyae_toy/LyaeToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyae.lyae_toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyaeToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
